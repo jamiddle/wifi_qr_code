ae65e66519700814a4184531c3808786e8a0641e
James <54590378+jamiddle@users.noreply.github.com>
2020-02-24 11:50:15 +0000

    creating png file to render the qr code

---
+++ wifi_qr_code_generator/wifi_qr_code_generator/cli.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+import pyqrcode as pq
+
+import click
+
+from .functions import wifi_qr, qr2array
+
+
+@click.group()
+@click.option('--ssid', help='WiFi network name (ssid).')
+@click.option('--security', type=click.Choice(['WEP', 'WPA', '']))
+@click.option('--password', help='WiFi password.')
+@click.pass_context
+def main(ctx, ssid: str, security: str = '', password: str = ''):
+    qr = wifi_qr(ssid=ssid, security=security, password=password)
+    ctx.obj['qr'] = qr
+
+
+@main.command()
+@click.pass_context
+def terminal(ctx):
+    """Print QR code to the terminal."""
+    print(ctx.obj['qr'].terminal())
+
+
+@main.command()
+@click.option('--filename', help='full path to the png file')
+@click.pass_context
+def png(ctx, filename, scale: int = 10):
+    """Create a PNG file of the QR code."""
+    ctx.obj['qr'].png(filename, scale)
